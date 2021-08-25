572129de62396200f66d60dd20dd38e30ca41117
Anthony Zamora <anthony.zamora@gmail.com>
2021-08-25 12:37:10 -0700

    test tables working

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO department (dept_name)
+VALUES ('Accounting'), ('Engineering'), ('Marketing'), ('Sales');
+
+INSERT INTO job (title, salary, department_id)
+VALUES ('Accountant', 100000, 1), ('Engineer', 150000, 2), ('Manager', 200000, 3), ('Salesman', 75000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('John', 'Doe', 1, NULL), ('Jane', 'Doe', 2, NULL), ('Bob', 'Smith', 3, NULL), ('Sally', 'Smith', 4, NULL);
