613e108d569bffe33e1692e2e752510047530060
Anthony Zamora <anthony.zamora@gmail.com>
2021-08-25 13:44:14 -0700

    added figlet and changed to employee db

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,6 @@
-DROP DATABASE IF EXISTS company_db;
-CREATE DATABASE company_db;
-USE company_db;
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+USE employee_db;
 
 DROP TABLE IF EXISTS department;
 DROP TABLE IF EXISTS job;
