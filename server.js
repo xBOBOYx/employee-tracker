b620078c961e22ef2cddbf63b299bf59b58130f7
Anthony Zamora <anthony.zamora@gmail.com>
2021-08-31 12:57:39 -0700

    server connection

--- server.js
+++ server.js
@@ -1,4 +1,24 @@
-var figlet = require('figlet');
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const cTable = require('console.table');
+const figlet = require('figlet');
+const chalk = require('chalk');
+
+require('dotenv').config()
+
+// connection to database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: process.env.DB_PASSWORD,
+  database: 'employee_db'
+});
+
+  connection.connect(err => {
+    if (err) throw err;
+    console.log(chalk.blue("You are now connected to the employee database"));
+    
+  });
 
 figlet('EMPLOYEE TRACKER', function(err, data) {
     if (err) {
